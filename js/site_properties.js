2c231637ce983f7a75dd6edea9ee6b6c90098bb3
namancg <naman.c@perfios.com>
2021-10-21 01:36:05 +0530

    REFACTORED: server URL's

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,4 +1,6 @@
 let site_properties = {
     home_page: "../pages/home.html",
     add_contact_page: "../pages/AddressBookForm.html",
+    use_local_storage: true,
+    server_url: "http://localhost/AddressDB/",
   };
