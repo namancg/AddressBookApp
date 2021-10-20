6594f1a51ce5ee6ac1319edc8573be9ea09045d0
namancg <naman.c@perfios.com>
2021-10-21 02:44:07 +0530

    REFACTORED: use_local_storage updated

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,6 +1,6 @@
 let site_properties = {
     home_page: "../pages/home.html",
     add_contact_page: "../pages/AddressBookForm.html",
-    use_local_storage: true,
-    server_url: "http://localhost/AddressDB/",
+    use_local_storage: "false",
+    server_url: "http://localhost:3000/AddressDB/",
   };
