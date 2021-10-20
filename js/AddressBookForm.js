ac2f7a4763bcf2375a314b7ccd3b73e9264671c3
namancg <naman.c@perfios.com>
2021-10-20 18:53:26 +0530

    REFACTORED:necessary functions added

--- js/AddressBookForm.js
+++ js/AddressBookForm.js
@@ -1,66 +1,66 @@
 let addressBookContactJsonObj = {};
 
-window.addEventListener('DOMContentLoaded', () =>{
+window.addEventListener('DOMContentLoaded', () => {
     const name = document.querySelector('#name');
     const textError = document.querySelector('.text-error');
-    name.addEventListener('input', function() {
-        if(name.value.length == 0){
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
             textError.textContent = "";
             return;
         }
-        try{
+        try {
             (new AddressBookContact()).name = name.value;
             textError.textContent = "";
-        } 
-        catch(e) {
+        }
+        catch (e) {
             textError.textContent = e;
         }
     });
 
     const phone = document.querySelector('#phone');
     const phoneError = document.querySelector('.phone-error');
-    phone.addEventListener('input', function() {
-        if(phone.value.length == 0){
+    phone.addEventListener('input', function () {
+        if (phone.value.length == 0) {
             phoneError.textContent = "";
             return;
         }
-        try{
+        try {
             (new AddressBookContact()).phoneNumber = phone.value;
             phoneError.textContent = "";
-        } 
-        catch(e) {
+        }
+        catch (e) {
             phoneError.textContent = e;
         }
     });
 
     const address = document.querySelector('#address');
     const addressError = document.querySelector('.address-error');
-    address.addEventListener('input', function() {
-        if(address.value.length == 0){
+    address.addEventListener('input', function () {
+        if (address.value.length == 0) {
             addressError.textContent = "";
             return;
         }
-        try{
+        try {
             (new AddressBookContact()).address = address.value;
             addressError.textContent = "";
-        } 
-        catch(e) {
+        }
+        catch (e) {
             addressError.textContent = e;
         }
     });
 
     const zip = document.querySelector('#zip');
     const zipError = document.querySelector('.zip-error');
-    zip.addEventListener('input', function() {
-        if(zip.value.length == 0){
+    zip.addEventListener('input', function () {
+        if (zip.value.length == 0) {
             zipError.textContent = "";
             return;
         }
-        try{
+        try {
             (new AddressBookContact()).zip = zip.value;
             zipError.textContent = "";
-        } 
-        catch(e) {
+        }
+        catch (e) {
             zipError.textContent = e;
         }
     });
@@ -69,12 +69,12 @@ window.addEventListener('DOMContentLoaded', () =>{
 const save = (event) => {
     event.preventDefault();
     event.stopPropagation();
-    try{
+    try {
         setaddressBookContactJsonObj();
-        setContactData(); 
+        createAndUpdateStorage();
         resetForm();
     }
-    catch(e){
+    catch (e) {
         return;
     }
 }
@@ -93,45 +93,58 @@ const setContactData = () => {
     let contact = new AddressBookContact();
     contact.id = addressBookContactJsonObj.id;
     const textError = document.querySelector('.text-error');
-    try{
+    try {
         contact._name = addressBookContactJsonObj._name;
     }
-    catch(e){
+    catch (e) {
         textError.textContent = e;
         throw e;
     }
     const phoneError = document.querySelector('.phone-error');
-    try{
+    try {
         contact._phoneNumber = addressBookContactJsonObj._phoneNumber;
     }
-    catch(e){
+    catch (e) {
         phoneError.textContent = e;
         throw e;
     }
     const addressError = document.querySelector('.address-error');
-    try{
+    try {
         contact._address = addressBookContactJsonObj._address;
     }
-    catch(e){
+    catch (e) {
         addressError.textContent = e;
         throw e;
     }
     contact._city = addressBookContactJsonObj._city;
     contact._state = addressBookContactJsonObj._state;
     const zipError = document.querySelector('.zip-error');
-    try{
+    try {
         contact._zip = addressBookContactJsonObj._zip;
     }
-    catch(e){
+    catch (e) {
         zipError.textContent = e;
         throw e;
     }
     alert(contact.toString());
-    localStorage.setItem("AddressBookList",JSON.stringify(addressBookContactJsonObj))
+    return contact;
+}
+const createAndUpdateStorage = () => {
+    let addressBookContactList = JSON.parse(localStorage.getItem("AddressBookContactList"));
+    if (addressBookContactList) {
+        addressBookContactList.push(setContactData());
+    }
+    else {
+        addressBookContactList = [setContactData()];
+    }
+    localStorage.setItem("AddressBookList", JSON.stringify(addressBookContactList));
 }
 
 const createContactId = () => {
-    return new Date().getTime();
+    let contactID = localStorage.getItem("ContactID");
+    contactID = !contactID ? "1" : (parseInt(contactID) + 1).toString();
+    localStorage.setItem("ContactID", contactID);
+    return contactID;
 }
 
 const getValueById = (value) => {
@@ -139,12 +152,12 @@ const getValueById = (value) => {
 }
 
 const resetForm = () => {
-    setValue('#name','');
-    setValue('#phone','');
-    setValue('#address','');
-    setValue('#city','');
-    setValue('#state','');
-    setValue('#zip','');
+    setValue('#name', '');
+    setValue('#phone', '');
+    setValue('#address', '');
+    setValue('#city', '');
+    setValue('#state', '');
+    setValue('#zip', '');
 }
 
 const setValue = (id, value) => {
