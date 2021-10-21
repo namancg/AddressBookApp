2ec4c076a884b7d74414582f9a9f7676a3c2e340
namancg <naman.c@perfios.com>
2021-10-21 06:06:34 +0530

    REFACTORED: removed _ from all attributes

--- js/AddressBookContact.js
+++ js/AddressBookContact.js
@@ -1,49 +1,49 @@
 class AddressBookContact{
     id;
-    get name() {return this._name; }
+    get name() {return this.name; }
     set name(name){
         let nameRegex = RegExp('^[A-Z][a-zA-Z]{2,}([ ][A-Z][a-zA-Z]*)?$');
         if(nameRegex.test(name)) 
-            this._name = name;
+            this.name = name;
         else throw "Name is Incorrect";
     }
 
-    get address() { return this._address; }
+    get address() { return this.address; }
     set address(address) {
         let addressRegex = RegExp('[a-zA-Z]{3,}(([ ][a-zA-Z]{3,})+)');
         if(addressRegex.test(address))
-        this._address = address;
+        this.address = address;
         else throw "Address is Incorrect";
     }
 
-    get phoneNumber() { return this._phoneNumber; }
+    get phoneNumber() { return this.phoneNumber; }
     set phoneNumber(phoneNumber) {
         let phoneNumberRegex = RegExp('^(([+]?[1-9][0-9])?)([6-9][0-9]{9})$');
         if(phoneNumberRegex.test(phoneNumber))
-            this._phoneNumber = phoneNumber;
+            this.phoneNumber = phoneNumber;
         else throw "Phone number is incorrect";
     }
     
-    get city() { return this._city; }
+    get city() { return this.city; }
     set city(city) {
-        this._city = city;
+        this.city = city;
     }
 
-    get state() { return this._state; }
+    get state() { return this.state; }
     set state(state) {
-        this._state = state;
+        this.state = state;
     }
 
-    get zip() { return this._zip; }
+    get zip() { return this.zip; }
     set zip(zip) {
         let zipRegex = RegExp('(^[0-9]{3})([ ]?)([0-9]{3}$)')
         if(zipRegex.test(zip))
-            this._zip = zip;
+            this.zip = zip;
         else throw "Zip Code is wrong";
     }
 
     toString() {
-        return "Name: "+ this._name+", PhoneNumber: "+ this._phoneNumber+ ", Address: "+ this._address+", City: "+ this._city + ", State: "+ this._state + 
-                ", Zip Code: "+ this._zip;
+        return "Name: "+ this.name+", PhoneNumber: "+ this.phoneNumber+ ", Address: "+ this.address+", City: "+ this.city + ", State: "+ this.state + 
+                ", Zip Code: "+ this.zip;
     }
 }
